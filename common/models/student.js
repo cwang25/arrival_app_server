80354203355cf6e573baec744be153461b37cef5
Hans <cwang25@ncsu.edu>
2016-04-14 05:03:50 -0400

    bug fix  typoe in attrubute

--- common/models/student.js
+++ common/models/student.js
@@ -2,28 +2,7 @@
  * Created by hanswang on 3/20/16.
  */
 module.exports = function(Student) {
-  var cloudantURL = "https://828ae6b0-5855-4cdb-9bc9-75bf1dfcd4fc-bluemix.cloudant.com/arrivaltest/_find";
-  var example_JSON = {
-    "use_index": "_design/lb-index-ddoc-Student",
-    "selector": {
-      "first_name": {
-        "$eq": "Test_first_name"
-      },
-      "_id":{
-        "$gt": null
-      }
-    },
-    "fields": [
-      "_id",
-      "school_email",
-      "sleep_time"
-    ],
-    "sort": [
-      {
-        "_id:string": "asc"
-      }
-    ]
-  };
+
   //
   //module.exports = function(CoffeeShop) {
   //  ...
@@ -63,8 +42,9 @@ module.exports = function(Student) {
       console.log(response);
     });
   };
-  Student.removeAllStudents = function(cd){
+  Student.removeAllStudents = function(cb){
     Student.destroyAll(function(err, info){
+      var response;
       response = info;
       cb(null, response);
       console.log(response);

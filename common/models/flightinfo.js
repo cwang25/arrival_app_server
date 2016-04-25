78ecce89c04a4be69b07abdafece0af5ea6b8482
Vamsi Vikash <vamsivikash@gmail.com>
2016-04-24 23:24:16 -0400

    changes

--- common/models/flightinfo.js
+++ common/models/flightinfo.js
@@ -79,7 +79,7 @@ module.exports = function(FlightInfo){
 		
 		console.log("FLIGHT NUMBER: " +fltNum);
 		console.log("FLIGHT DATE: " +fltDate);
-		FlightInfo.find({where: [{flightNum:fltNum}, {flightDate: fltDate}]}, fields:{student_id:true}}, function(err, instance){
+		FlightInfo.find({where: [{flightNum:fltNum}, {flightDate: fltDate}]}, fields:{student_id:true}, function(err, instance){
 			//FlightInfo.find({where: {and: [{flightNum:fltNum}, {flightDate: fltDate}]}, fields:{student_id:true}},function(err, instance){
 			var response;
 			response = instance;
